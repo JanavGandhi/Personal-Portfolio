1d57ed927c0575e862fede95253fc9b0ff75bf7c
Janav Gandhi <141824807+JanavGandhi@users.noreply.github.com>
2024-11-13 00:10:43 +0530

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+$('.slider').slick({
+    arrows:false,
+    dots:true,
+    appendDots:'.slider-dots',
+    dotsClass:'dots'
+});
+
+
+let hamberger = document.querySelector('.hamberger');
+let times = document.querySelector('.times');
+let mobileNav = document.querySelector('.mobile-nav');
+
+hamberger.addEventListener('click', function(){
+  mobileNav.classList.add('open');  
+});
+
+times.addEventListener('click', function(){
+    mobileNav.classList.remove('open');  
+});
+
+});
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
